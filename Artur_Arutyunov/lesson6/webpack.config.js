984cde3cd4614c2d0a56d2b358f7904738a716ec
Artur Arutyunov <aarutyunov852005@gmail.com>
2022-03-15 22:31:00 +0300

    lesson6

---
+++ Artur_Arutyunov/lesson6/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
+// const { resolve } = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'webpack.bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+        components: path.join(__dirname, 'src', 'components'),
+        config: path.join(__dirname, 'src', 'config'),
+        pages: path.join(__dirname, 'src', 'pages'),
+        hooks: path.join(__dirname, 'src', 'hooks'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: { '^/api': '' },
+        secure: false,
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      title: 'App v1',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+    }),
+    new Dotenv(),
+  ],
+  //   externals: {
+  //     Config: JSON.stringify(
+  //       process.env.npm_lifecycle_event === 'dev'
+  //         ? require('./src/config/config.development.env.json')
+  //         : require('./src/config/config.production.env.json'),
+  //     ),
+  //   },
+}
