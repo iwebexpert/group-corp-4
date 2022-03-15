9847187a29d5b115a16f6565d84f9ab885c48fe4
Ancho001 <100517300+Ancho001@users.noreply.github.com>
2022-03-15 16:19:40 +0300

    Homework4

---
+++ AnnUkraintseva/react-app/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'webpack.bundle.js',
+  },
+
+  resolve: {
+    alias: {
+      components: path.join(__dirname, 'src', 'components'),
+      redux: path.join(__dirname, 'src', 'redux')
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: { '^/api': '' },
+        secure: false,
+      },
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      title: 'AnnUkraintseva APP1',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+    }),
+
+    new Dotenv(),
+  ],
+}
