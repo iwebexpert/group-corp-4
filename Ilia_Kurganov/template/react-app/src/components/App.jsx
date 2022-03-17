5f0910fd9e59803e5accc3196e16444bbed85c22
Курганов Илья Вячеславович <IKurganov@ibs.root.ibscorp.ru>
2022-03-17 21:24:28 +0300

    react

--- Ilia_Kurganov/template/react-app/src/components/App.jsx
+++ Ilia_Kurganov/template/react-app/src/components/App.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PageTable from './PageTable/PageTable'
+import PageForm from './PageForm/PageForm'
+
+import './App.scss'
 
 export const App = () => {
-    return (
-        <div>Ji</div>
-    )
+  const [tableRows, setTableRows] = useState([])
+
+  const addRows = (row) => {
+    setTableRows((rows) => {
+      return [...rows, row]
+    })
+  }
+
+  const delitePage = (key) => {
+    const filterPage = tableRows.filter((page) => page.id !== key)
+    setTableRows(filterPage)
+  }
+
+  return (
+    <>
+      <div className='inner'>
+        <PageTable tableRows={tableRows} delitePage={delitePage} />
+        <PageForm addRows={addRows} />
+      </div>
+    </>
+  )
 }
--- Ilia_Kurganov/template/react-app/src/index.js
+++ Ilia_Kurganov/template/react-app/src/index.js
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 import './style.scss'
-import {App} from 'components/App'
-
-const Config = require('Config');
+import { App } from './components/App'
+import { ThemeContext } from './context/ThemeContext'
+// const Config = require('Config');
 
 ReactDOM.render(
   <>
-    <App/>
-    <div>REACT_APP_BASE_URL : {process.env.REACT_APP_BASE_URL}</div>
-    <div>DB_PASS : {process.env.DB_PASS}</div>
-    <div>REACT_APP_BASE_URL : {Config.REACT_APP_BASE_URL}</div>
-    <div>REACT_APP_THEME : {Config.REACT_APP_THEME}</div>
+    <ThemeContext>
+      <App />
+    </ThemeContext>
+    {/* <div>REACT_APP_BASE_URL : {process.env.REACT_APP_BASE_URL}</div>
+    <div>REACT_APP_THEME : {Config.REACT_APP_THEME}</div> */}
   </>,
   document.querySelector('#root'),
 )
