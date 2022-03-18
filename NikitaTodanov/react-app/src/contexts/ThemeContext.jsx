9e93e2f49da9aa7d08b61df36edcf25435c2f992
Nikita <65577813+Todanov@users.noreply.github.com>
2022-03-18 08:26:53 +0300

    create-theme-context

---
+++ NikitaTodanov/react-app/src/contexts/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import React,{useContext,Component} from 'react';
+const {Provider,Consumer}=React.createContext()
+
+class ThemeContext extends Component {
+    state = {
+        theme: 'light'
+    }
+    toggleTheme =() => {
+        this.setState({theme: this.state.theme === 'light' ? 'dark': 'light'})
+    }
+    render() {
+        return (
+            <Provider value={{theme:this.state.theme, toggleTheme:this.toggleTheme}}>
+                {this.props.children}
+            </Provider>
+        )
+    }
+}
+export {ThemeContext as ThemeContextProvider, Consumer  as ThemeContextConsumer}
