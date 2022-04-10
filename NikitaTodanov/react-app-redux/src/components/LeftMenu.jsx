c96630ec87b188e7dd37ddb7d5d36a9621fbd413
Nikita <65577813+Todanov@users.noreply.github.com>
2022-04-10 12:02:43 +0300

    Add files via upload

--- NikitaTodanov/react-app-redux/src/components/LeftMenu.jsx
+++ NikitaTodanov/react-app-redux/src/components/LeftMenu.jsx
@@ -3,11 +3,14 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
 import AddIcon from "@mui/icons-material/Add";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import LogoutIcon from "@mui/icons-material/Logout";
-import InfoIcon from '@mui/icons-material/Info';
+import InfoIcon from "@mui/icons-material/Info";
+import CommentIcon from "@mui/icons-material/Comment";
+import PeopleIcon from "@mui/icons-material/People";
 
 import { Link } from "react-router-dom";
 
@@ -31,11 +34,55 @@ export const secondaryListItems = (
       </ListItemIcon>
       <ListItemText primary="Профиль" />
     </ListItemButton>
+
+    <ListItemButton component={Link} to="/pages">
+      <ListItemIcon>
+        <AutoStoriesIcon />
+      </ListItemIcon>
+      <ListItemText primary="Страницы" />
+    </ListItemButton>
+    <ListItemButton component={Link} to="/users">
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Пользователи" />
+    </ListItemButton>
+    <ListItemButton component={Link} to="/comments">
+      <ListItemIcon>
+        <CommentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Коментарии" />
+    </ListItemButton>
     <ListItemButton component={Link} to="/stats">
       <ListItemIcon>
         <InfoIcon />
       </ListItemIcon>
       <ListItemText primary="Логи" />
+    </ListItemButton>
+
+    <ListItemButton component={Link} to="/logout">
+      <ListItemIcon>
+        <LogoutIcon />
+      </ListItemIcon>
+      <ListItemText primary="Выход" />
+    </ListItemButton>
+  </>
+);
+
+export const secondaryListItemsUser = (
+  <>
+    <ListSubheader component="div" inset></ListSubheader>
+    <ListItemButton component={Link} to="/profile">
+      <ListItemIcon>
+        <AccountBoxIcon />
+      </ListItemIcon>
+      <ListItemText primary="Профиль" />
+    </ListItemButton>
+    <ListItemButton component={Link} to="/comments">
+      <ListItemIcon>
+        <CommentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Коментарии" />
     </ListItemButton>
     <ListItemButton component={Link} to="/logout">
       <ListItemIcon>
